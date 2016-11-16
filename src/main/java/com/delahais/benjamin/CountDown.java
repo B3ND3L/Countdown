5e2a1d49d8fe21ab3efed4cd610049d35478a751
lemazo <delahais.b@gmail.com>
2016-11-16 15:26:36 +0100

    Suppression des cookies (DOESN'T WORK)

--- src/main/java/com/delahais/benjamin/CountDown.java
+++ src/main/java/com/delahais/benjamin/CountDown.java
@@ -17,35 +17,25 @@ public class CountDown extends HttpServlet {
 	
 	private WebSocketServer connector;
 	private static CountDown CD;
-	private HashMap<String, ArrayList<Cookie>> usersCookies;
-	private HashMap<String, ArrayList<Compteur>> users;
+	private DataBase db;
 	private SecureRandom random = new SecureRandom();
 
 	public CountDown() {
 		
 		connector = new WebSocketServer();
-		
-		users = new HashMap<String,ArrayList<Compteur>>();
-		
-		usersCookies = new HashMap<String,ArrayList<Cookie>>(); 
-		
+				
 		CD = this;
 	}
 
 	public ArrayList<Compteur> getCompteurs(String user){
-		return users.get(user);
+		//SELECTION BASE DE DONNEE
+		return null;
 	}
 	
 	public void addCompteur(String user, String json){
 		
 		Compteur cpt = Compteur.JSONtoCompteur(json);
-		
-		Cookie cookie = new Cookie("compteurs"+cpt.getId(), cpt.toJSON(null).toString());
-		cookie.setMaxAge(60*60*24*365); //Store cookie for 1 year
-		usersCookies.get(user).add(cookie);
-		
-		System.out.println("creation du nouveau compteur");
-		users.get(user).add(cpt);
+		//Envoi du compteur dans la BDD
 	}
 	
 	@Override
@@ -57,27 +47,15 @@ public class CountDown extends HttpServlet {
 		//Si des cookies existent, on les lit
 		if(cooks != null && cooks.length >= 2){
 			userId = cooks[1].getValue();
-			for(int i = 2; i<cooks.length;i++){
-				users.get(userId).add(Compteur.JSONtoCompteur(cooks[i].getValue()));
-			}
 		//Sinon c'est que c'est la première connection du client
 		} else {
 			userId = nextSessionId();
-			users.put(userId, new ArrayList<Compteur>());
-			usersCookies.put(userId, new ArrayList<Cookie>());
 			response.addCookie(new Cookie("userId", userId));
 		}
-				
-		//Pour tous cookies préparés on les envoie au navigateur
-		if(usersCookies.get(userId) != null){
-			for(Cookie c : usersCookies.get(userId)){
-				System.out.println(c);
-				response.addCookie(c);
-			}
-			usersCookies.get(userId).clear();
-		}
 		
-		request.setAttribute("compteurs", users.get(userId));
+		//RECUPERATION DES COMPTEURS PAR LA BDD
+		//request.setAttribute("compteurs", users.get(userId));
+		
 		request.setAttribute("userId", userId);
 		request.setAttribute("ip", InetAddress.getLocalHost().getHostAddress());
 		
